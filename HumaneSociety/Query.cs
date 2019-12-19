e10363be422ec12c177fc766eaba2bdeb1db9f95
Elias Holter <eliasholter1@gmail.com>
2019-12-19 09:30:30 -0600

    Actually pushing the GetRoom function, didn't get pushed to repo last push

--- HumaneSociety/Query.cs
+++ HumaneSociety/Query.cs
@@ -207,7 +207,8 @@ namespace HumaneSociety
         
         internal static Room GetRoom(int animalId)
         {
-            throw new NotImplementedException();
+            var room = db.Rooms.Where(r => r.AnimalId == animalId).FirstOrDefault();
+            return room;
         }
         
         internal static int GetDietPlanId(string dietPlanName)
